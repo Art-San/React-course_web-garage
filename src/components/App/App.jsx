0cde639f21d4956d8674552e5566ef68bf58fb88
Art San <artsan.mcc@gmail.com>
2025-03-24 17:17:43 +0700

    ok

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,14 @@
-import { MyName } from '../myName/MyName'
+import { MyName } from '../MyName/myName'
 import './App.css'
 
+const test = 1
 function App() {
   return (
     <>
       <div className="">
         <h1>Привет реакт</h1>
         <p>Это мой первый</p>
+        {test}
         <MyName />
       </div>
     </>
