5c21e9df85951a177cb501c1fde43222241cfe73
Art San <artsan.mcc@gmail.com>
2025-03-27 17:38:25 +0700

    ok

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,18 @@
 import './HomePage.css'
 
 function HomePage() {
-  return <div>Домашняя страница</div>
+  return (
+    <>
+      <div>Домашняя страница</div>
+      <button
+        onClick={() => {
+          alert('кнопка нажата')
+        }}
+      >
+        Кнопку нажать
+      </button>
+    </>
+  )
 }
 
 export default HomePage
